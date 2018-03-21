c346e59a8edbde47476195251b6145badbf0f7de
whoami <vuong.tan.nhac@gmail.com>
2018-03-21 15:22:30 +0700

    week
    #Week of Month

---
+++ Week/frmWeek.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Test
+{
+    partial class frmWeek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtpBegindate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dtpBegindate
+            // 
+            this.dtpBegindate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBegindate.Location = new System.Drawing.Point(116, 9);
+            this.dtpBegindate.Name = "dtpBegindate";
+            this.dtpBegindate.Size = new System.Drawing.Size(105, 20);
+            this.dtpBegindate.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chọn ngày bắt đầu";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(273, 276);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Vương Tấn  Nhạc";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 323);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpBegindate);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Quản lý hồ sơ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dtpBegindate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Week/frmWeek.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace Test
+{
+    public partial class frmWeek : Form
+    {
+        //Khai báo biến toàn cục 
+
+        public frmWeek()
+        {
+            InitializeComponent();
+        }
+
+        public void WeekOfMonth(DateTime a)
+        {
+            string day = a.DayOfWeek.ToString();
+            int year = a.Year;
+            int mouth = a.Month;
+            int plag = 0;
+            DateTime dtx = new DateTime(year, mouth + 1, 1);
+            TimeSpan ax = new TimeSpan(-1);
+            TimeSpan axs = new TimeSpan(1);
+            DateTime cuoithang = dtx.Add(ax);
+
+            string text = "";
+            switch (day)
+            {
+                case "Monday":
+                    plag = 2;
+                    break;
+                case "Tuesday":
+                    plag = 3;
+                    break;
+                case "Wednesday":
+                    plag = 4;
+                    break;
+                case "Thursday":
+                    plag = 5;
+                    break;
+                case "Friday":
+                    plag = 6;
+                    break;
+                case "Saturday":
+                    plag = 7;
+                    break;
+                default:
+                    plag = 1;
+                    break;
+            }
+            int dem = 0;
+            int ngay = 0;
+            for (int i = a.Day; i <= cuoithang.Day; i++)
+            {
+                DateTime dti = new DateTime(year, mouth, i);
+                int wk = GetWeekOfMonth(dti);
+                if (wk == 1)
+                {
+                    text += String.Format(@"Tuần {0} " + Environment.NewLine, wk);
+                    dem = 0;
+                    for (int j = plag; j <= 7; j++)
+                    {
+                        dem++;
+                        text += String.Format(@"{0}    ", dem);
+                    }
+                    i += dem - 1;
+                    ngay = i;
+                }
+                else if (wk == 2)
+                {
+                    text += String.Format(Environment.NewLine + @"Tuần {0} " + Environment.NewLine, wk);
+                    dem = 0;
+                    for (int j = i; j <= ngay + 7; j++)
+                    {
+                        text += String.Format(@"{0}    ", j);
+                        dem++;
+                    }
+                    i += dem - 1;
+                    ngay = i;
+                }
+                else if (wk == 3)
+                {
+                    text += String.Format(Environment.NewLine + @"Tuần {0} " + Environment.NewLine, wk);
+                    dem = 0;
+                    for (int j = i; j <= ngay + 7; j++)
+                    {
+                        text += String.Format(@"{0}    ", j);
+                        dem++;
+                    }
+                    i += dem - 1;
+                    ngay = i;
+                }
+                else if (wk == 4)
+                {
+                    text += String.Format(Environment.NewLine + @"Tuần {0} " + Environment.NewLine, wk);
+                    dem = 0;
+                    for (int j = i; j <= ngay + 7; j++)
+                    {
+                        text += String.Format(@"{0}    ", j);
+                        dem++;
+                    }
+                    i += dem - 1;
+                    ngay = i;
+                }
+                else if (wk == 5)
+                {
+                    text += String.Format(Environment.NewLine + @"Tuần {0} " + Environment.NewLine, wk);
+                    dem = 0;
+                    for (int j = i; j <= ngay + 7; j++)
+                    {
+                        text += String.Format(@"{0}    ", j);
+                        dem++;
+                        if ((i + dem) > cuoithang.Day)
+                            break;
+                    }
+                    i += dem - 1;
+                    ngay = i;
+                }
+                else if (wk == 6)
+                {
+                    text += String.Format(Environment.NewLine + @"Tuần {0} " + Environment.NewLine, wk);
+                    dem = 0;
+                    for (int j = i; j <= ngay + 7; j++)
+                    {
+                        text += String.Format(@"{0}    ", j);
+                        dem++;
+                        if ((i + dem) > cuoithang.Day)
+                            break;
+                    }
+                    i += dem - 1;
+                    ngay = i;
+                }
+            }
+            richTextBox1.Text = text.ToString();
+        }
+        public static int GetWeekOfMonth(DateTime date)
+        {
+            DateTime beginningOfMonth = new DateTime(date.Year, date.Month, 1);
+
+            while (date.Date.AddDays(1).DayOfWeek != CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek)
+                date = date.AddDays(1);
+
+            return (int)Math.Truncate((double)date.Subtract(beginningOfMonth).TotalDays / 7f) + 1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dt = new DateTime(dtpBegindate.Value.Year, dtpBegindate.Value.Month, 1);
+            WeekOfMonth(dt);
+        }
+    }
+}
